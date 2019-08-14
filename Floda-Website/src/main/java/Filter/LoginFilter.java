273a725b2d0d20473262f48688416d01351c9991
Zayvion_Lee <me@lzllzl.cn>
2019-08-14 08:42:56 +0800

    增加前后台登录过滤器。

---
+++ Floda-Website/src/main/java/Filter/LoginFilter.java
@@ -0,0 +1,39 @@
+package Filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Auther: zayvion
+ * @Date: 2019-08-13 14:20
+ * @Description:登录过滤器
+ */
+
+public class LoginFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+        HttpServletResponse httpResponse = (HttpServletResponse)response;
+        Object flag = httpRequest.getSession().getAttribute("userFlag");
+        if (flag != null){
+            chain.doFilter(request,response);
+        }else {
+            httpResponse.sendRedirect("http://localhost:8080/login.jsp");
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
